692cbe0f33fecc0efb5be6a444ea8825cf1908df
AjayKamath10 <ajayvkamath10@gmail.com>
2024-01-17 13:49:45 +0530

    Assignment 1

---
+++ ProductClass/ProductClass/Product.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ProductClass
+{
+    internal class Product
+    {
+        private int PrivateProductId;
+        private string PrivateName;
+        private DateTime PrivateMfgDate;
+        private int PrivateWarranty;
+        private decimal PrivatePrice;
+        private int PrivateStock;
+        private decimal PrivateDiscount;
+        private int PrivateGST;
+
+        public int ProductId
+        {
+            get
+            {
+                return PrivateProductId;
+            }
+            set
+            {
+                if (value > 0)
+                    PrivateProductId = value;
+                else
+                    throw new Exception("Invalid Employee ID");
+            }
+        }
+              
+
+        public string ProductName
+        {
+            get { return PrivateName; }
+            set
+            {
+                if (value.Length>3)
+                {
+                    PrivateName = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid Name");
+                }
+            }
+        }
+
+        public DateTime MfgDate
+        {
+            get { return PrivateMfgDate; }
+            set
+            {
+                if (value <= DateTime.Now)
+                {
+                    PrivateMfgDate = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid Date");
+                }
+            }
+        }
+
+        public int Warranty
+        {
+            get { return PrivateWarranty; }
+            set { PrivateWarranty = value; }
+        }
+
+        public decimal Price
+        {
+            get { return PrivatePrice; }
+            set
+            {
+                if (value > 0)
+                {
+                    PrivatePrice = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid Price");
+                }
+            }
+        }
+
+        public int Stock
+        {
+            get { return PrivateStock; }
+            set
+            {
+                if (value >= 0)
+                {
+                    PrivateStock = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid Stock");
+                }
+            }
+        }
+
+        public int GST
+        {
+            get { return PrivateGST; }
+            set
+            {
+                List<int> AllowedGstValues = new List<int>([5,12,18,28]);
+                if (AllowedGstValues.Contains(value))
+                {
+                    PrivateGST = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid GST Value");
+                }
+            }
+        }
+
+        public decimal Discount
+        {
+            get { return PrivateDiscount; }
+            set
+            {
+                if (value >= 0 && value <= 30)
+                {
+                    PrivateDiscount = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid Discount");
+                }
+            }
+        }
+
+        private decimal TaxPrice, DiscountPrice;
+        public string Display()
+        {
+            TaxPrice = Price * (1 + GST / 100);
+            DiscountPrice = TaxPrice * (1-Discount/100);
+
+            StringBuilder Details = new StringBuilder();
+            Details.Append("Id: " + ProductId + "\n");
+            Details.Append("Name: " + ProductName + "\n");
+            Details.Append("Manufacturing Date: " + MfgDate.ToShortDateString() + "\n");
+            Details.Append("Warranty: " + Warranty + " months\n");
+            Details.Append("Tax Price: " + TaxPrice.ToString() + "\n");
+            Details.Append("Stock: " + Stock + "\n");
+            Details.Append("Discount Price: " + DiscountPrice.ToString() + "\n");
+
+            return Details.ToString();
+        }
+
+
+    }
+
+
+}
---
+++ ProductClass/ProductClass/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductClass
+{
+
+    internal class Program
+    {
+        private int PrivateProductId;
+        private string PrivateName;
+        private DateTime PrivateMfgDate;
+        private int PrivateWarranty;
+        private decimal PrivatePrice;
+        private int PrivateStock;
+        private decimal PrivateDiscount;
+        private int PrivateGST;
+
+        static void Main(string[] args)
+        {
+
+            int choice;
+            var productList = new List<Product>();
+
+            do
+            {
+                Console.WriteLine("-----MENU-----");
+                Console.WriteLine();
+                Console.WriteLine("1. Add Product");
+                Console.WriteLine("2. Display Products");
+                Console.WriteLine("3. Exit");
+
+                Console.Write("Enter your choice: ");
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        AddProduct(productList);
+                        break;
+                    case 2:
+                        DisplayProducts(productList);
+                        break;
+                    case 3:
+                        Console.WriteLine("Exiting...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please enter a valid option.");
+                        break;
+                }
+
+                
+
+
+
+            } while (choice != 3);
+        }
+
+        static void AddProduct(List<Product> productList)
+        {
+            Product newProduct = new Product();
+            Console.Write("Enter Product ID: ");
+            newProduct.ProductId = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Product Name: ");
+            newProduct.ProductName = Console.ReadLine();
+
+            Console.Write("Enter Manufacturing Date (mm/dd/yyyy): ");
+            newProduct.MfgDate = Convert.ToDateTime(Console.ReadLine());
+
+            Console.Write("Enter Warranty(in months): ");
+            newProduct.Warranty = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Price: ");
+            newProduct.Price = Convert.ToDecimal(Console.ReadLine());
+        
+            Console.Write("Enter Stock: ");
+            newProduct.Stock = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter GST(5, 12, 18, or 28): ");
+            newProduct.GST = Convert.ToInt32(Console.ReadLine());
+        
+            Console.Write("Enter Discount: ");
+            newProduct.Discount = Convert.ToDecimal(Console.ReadLine());
+            
+            productList.Add(newProduct);
+    }
+
+        static void DisplayProducts(List<Product> ProductList)
+        {
+            Console.WriteLine("----PRODUCT DETAILS----");
+            Console.WriteLine();
+            foreach (Product TempProduct in ProductList)
+            {
+                Console.WriteLine("------------");
+                Console.WriteLine(TempProduct.Display());
+                
+                Console.WriteLine();
+            }
+            Console.WriteLine("------------");
+        }
+    }
+}
+
